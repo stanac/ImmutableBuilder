d9f496ecd5544896113331e42830b97c000c847f
Milan Stanacev <stanacevm@gmail.com>
2018-06-29 18:14:27 +0200

    fixing appvayor config

--- codecoverage.bat
+++ codecoverage.bat
@@ -4,4 +4,4 @@ dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
 nuget install coveralls.net -Version 0.7.0 -OutputDir nugettools
 
-.\nugettools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i coverage.opencover.xml --repoToken $env:COVERALL_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
+.\nugettools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i tests\ImmutableBuilder.Tests\coverage.opencover.xml --repoToken $env:COVERALL_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
